454dbe094ad0e2197f789bda8c606d9ce8227116
tcontrer <taylorcontreras@g.harvard.edu>
2021-04-18 11:51:25 -0400

    Updated from tests

--- bayboone/data_io.py
+++ bayboone/data_io.py
@@ -19,3 +19,61 @@ def get_example_data_file_path(filename, data_dir='example_data'):
 
 def load_data(data_file):
     return pd.read_csv(data_file, sep=' ')
+
+
+def generative_model(n_points=100, stheta=0.5, dm=10e-5):
+    """
+    Simulates data based on a generative model of the 4-neutrino
+    oscillation probability. 
+    
+    Inputs:
+        n_points: int
+            The number of data points to generate
+        stheta: float between 0 and 1
+            The oscillation parameter sin^2(2*theta_14)
+        dm: float
+            The oscillation parameter delta(m_14)^2
+            
+    Returns:
+        data: pandas dataframe
+            A dataframe holding the simulated events with columns 
+            for the number of electron neutrinos (N_nue), the 
+            number of muon neutrinos (N_numu), the beam 
+            length (L), the number of initial muon neutrinos 
+            (N_numu_initial), and the beam energy (E)
+    """
+    # Constants set by BNB and Microboone 
+    L = 500. # m
+    E_min = 0.2 # GeV # TODO: check with BNB low energy limit
+    E_max = 5.0 # GeV
+    E_mean = (E_min+E_max)/2.
+    initial_flux = 10e-4 # nu_mu's/POT/GeV/m^2 # TODO: check with BNB flux
+    
+    # Generate data points in energy
+    sigma = 1.0
+    E = stats.truncnorm(
+            (E_min - E_mean) / sigma, 
+            (E_max - E_mean) / sigma, 
+            loc=E_mean, scale=sigma).rvs(n_points)
+    
+    # Calculate the probability of oscillation
+    P = stheta * np.sin((L/E)*dm)**2
+    
+    # Calculate the number of muon and electron neutrinos
+    N_nue = initial_flux * P
+    N_numu = initial_flux * (1-P)
+    
+    # Create a data frame to hold the data
+    data = pd.DataFrame(dict(N_numu = N_numu,
+                        N_nue = N_nue,
+                        L = L*np.ones_like(N_numu),
+                        N_numu_initial = initial_flux*np.ones_like(N_numu), 
+                        E = E))
+    
+    return data
+
+def write_simulated_data(filename, data_dir='data', n_points=100, stheta=0.5, dm=10e-5):
+    data = generative_model(n_points)
+    data.to_csv(data_dir+'/'+file_name, index=False)
+    return
+
