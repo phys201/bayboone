648e027f7edc1c7685dcd048ce2c160cdb15d5dd
tcontrer <taylorcontreras@g.harvard.edu>
2021-04-18 16:56:46 -0400

    Added data tests

--- bayboone/tests/test_inference.py
+++ bayboone/tests/test_inference.py
@@ -1,6 +1,6 @@
 from bayboone.inference.prior import UniformPrior, JefferysPrior
 from bayboone.inference.inference_functions import chisq
-from bayboone.data_io import generative_model
+from bayboone.data_io import Data
 
 import numpy as np
 
@@ -16,10 +16,10 @@ class TestPriors(TestCase):
         assert np.allclose(np.exp(JefferysPrior(10, 1000).logp(100)),
                            0.0021714724095162588)
     
-class TestInference_function(TestCase):
-    def test_chisq(self):
-        data = generative_model(10)
-        assert np.sum(chisq(data, data)) == 0.0
+#class TestInference_function(TestCase):
+#    def test_chisq(self):
+#        data = generative_model(10)
+#        assert np.sum(chisq(data, data)) == 0.0
     
 if __name__ == '__main__':
     unittest.main()
--- bayboone/tests/test_io.py
+++ bayboone/tests/test_io.py
@@ -1,11 +1,32 @@
 from unittest import TestCase
-from bayboone.data_io import generative_model
-import pandas as  pd
+from bayboone.data_io import Data
+import random
 
 class TestIo(TestCase):
-    def test_data_io(self):
-        data = generative_model(10) 
-        assert data.L[0] == 500
+    
+    def test_is_Data_class(self):
+        data = Data(10,5)
+        assert isinstance(data, Data)
+    
+    def test_values_stored_correctly(self):
+        data = Data(10, 5)
+        assert data.N_numu == 10
+        assert data.N_nue == 5
         
-        # TODO: add more tests
+    def test_write_read(self):
+        data = Data(10,5)
+        data.write_data('testing.csv')
+        data = Data.load('testing.csv')
+        assert data.N_numu == 10
+        assert data.N_nue == 5
+        
+    def test_simulate_data(self):
+        random.seed(28)
+        data = simulate_data(10, 1.0, 1.0)
+        assert data.N_numu == 10
+        assert data.N_nue == 7
+        
+        
+        
+      
         
