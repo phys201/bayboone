abbd363ad96a3d158e0adb023ef01c5fac51d993
jybook <julia.y.book@gmail.com>
2021-04-21 01:14:32 -0400

    edits before due date #2
    Mostly addressing direct feedback from comments and pull requests, some more tuning of generative model

--- bayboone/inference/module.py
+++ bayboone/inference/module.py
@@ -15,7 +15,7 @@ def oscillation_model(num_neutrinos, num_nue):
         The number of electron neutrinos detected
     Returns:
     osc_model: pymc3 model
-        the statistical model for our neutrino oscillations
+        the statistical model for our neutrino oscillations in the 3+1 model
     '''
     #Check that the data is reasonable
     if (num_neutrinos < num_nue):
@@ -24,109 +24,70 @@ def oscillation_model(num_neutrinos, num_nue):
     osc_model = pm.Model()
     with osc_model:
     
-        # Priors for unknown model parameters
-        ss2t = pm.Uniform('sin^2_2theta', 0, 1)
-        dms = pm.Uniform('delta_m^2', 0, 1.0) #units of ev^2
-        # We don't know the exact energy or production point of each neutrino, so we draw from a truncated  gaussian (enforcing positive distance travelled and energy)
-        #L_over_E = pm.Normal('L_over_E', mu = 0.5, sigma = 0.1 ) #units of km/Gev
+        # Priors for unknown model parameters, covering the sensitivity region of modern experiments
+        ss2t = pm.Uniform('sin^2_2theta', 0.0001, 1)
+        #dms = pm.Uniform('delta_m^2', 0.01, 11.0) #units of ev^2
+        log_dms = pm.Uniform('log_delta_m^2', -4.605, 2.3)
+        # We don't know the exact energy or production point of each neutrino, so we draw from a truncated gaussian 
+        # (enforcing positive distance travelled and energy)
+        
         L = pm.TruncatedNormal('L', mu = 0.500, sigma = 0.05, lower = 0) #units of km
         E = pm.TruncatedNormal('E', mu = 1.0, sigma = 0.05, lower = 0) #units of GeV
 
-        # Expected value from theory 
-        #P = pm.Deterministic('prediction', ss2t*(np.sin(dms*(1.27*L)/E))**2)
-        # Likelihood of observations
-        # measurements = pm.Binomial('nue_Flux', n=num_neutrinos, p=P, observed=num_nue)
-        
         # In the large n limit, because the number of oscillations is low, we use a Poisson approximation
-        # Rate parameter calculated form theory
-        rate = pm.Deterministic('rate', num_neutrinos*ss2t*(np.sin(dms*(1.27*L)/E))**2)
+        #rate = pm.Deterministic('rate', num_neutrinos*ss2t*(np.sin(dms*(1.27*L)/E))**2)
+        rate = pm.Deterministic('rate', num_neutrinos*ss2t*(np.sin(np.exp(log_dms)*(1.27*L)/E))**2)
         
         #Likelihood of observations
         measurements = pm.Poisson('nue_flux', mu = rate, observed = num_nue)
         
     return osc_model
 
-def fit_model(data):
-    '''Fits a given model to data provided
-    Inputs:
-    data: two floats
-        the number of muon neutrinos produced (data[0]) and e- neutrinos detected (data[1])'''
+def fit_model(num_neutrinos, num_nue, num_draws = 1000):
+    '''
+    Fits a given model to data provided using MCMC sampling
     
-    num_neutrinos = data[0]
-    num_nue = data[1]
+    Inputs:
+    num_neutrinos: float
+        the number of muon neutrinos produced 
+    num_nue: number of electron neutrinos detected
+        e- neutrinos detected 
+    num_draws: int
+        number of samples to draw
+        
+    Returns:
+    trace: arviz InferenceData object
+        results of the mcmc sampling procedure
+        
+        '''
     uncertainty = np.sqrt(num_nue)
     osc_model = oscillation_model(num_neutrinos, num_nue)
     
     with osc_model:
-        trace = pm.sample(10000)
+        trace = pm.sample(num_draws)
         az.plot_trace(trace)
         
     return trace
-
-def chisq(fake_data, prediction):
-    '''
-    Finds chi-squared for a given model
-    
-    Parameters
-    --------
-    prediction: numpy array
-        the prediction from best-fit of for a given model 
-        (the best fit means this gives the minimum chi squared)
-    x_vals: numpy array
-        the y values for our data
-    
-    Returns
-    --------
-    float:
-        the chi-squared
-    '''   
-    uncertainty = 0.003
-    observed = fake_data['N_nue']
-    res_squared = np.power(prediction-observed, 2)
-    chisq = np.sum(res_squared/uncertainty**2)
-    
-    return chisq
                          
-def print_fit_vals(bf, cov):
+def print_fit_vals(trace):
     
     '''
     literally just prints the best fit values and their uncertainties nicely
     
     Inputs:
-    bf: best fit from the model
-    cov: covariance matrix from the model
+    trace: arviz InferenceData object
+        Holds the results from the mcmc sampling procedure
+    
     '''
-    points = len(cov[0])
-    values = np.zeros(points)
-    rows = np.full(points, '00000000000000')
-    uncertainty = np.zeros(points)
+    df_trace = pm.trace_to_dataframe(trace)
+    df_trace['delta_m^2'] = np.exp(df_trace['log_delta_m^2'])
+    q = df_trace.quantile([0.16,0.50,0.84], axis=0)
+    print("delta_m^2 = {:.2f} + {:.2f} - {:.2f}".format(q['delta_m^2'][0.50], 
+                                            q['delta_m^2'][0.84]-q['delta_m^2'][0.50],
+                                            q['delta_m^2'][0.50]-q['delta_m^2'][0.16]))
+    print("sin^2_2theta = {:.1f} + {:.1f} - {:.1f}".format(q['sin^2_2theta'][0.50], 
+                                            q['sin^2_2theta'][0.84]-q['sin^2_2theta'][0.50],
+                                            q['sin^2_2theta'][0.50]-q['sin^2_2theta'][0.16]))
     
-    values[0] = bf['sin^2_2theta']
-    rows[0] = 'sin^2_2theta'
-    uncertainty[0] = np.sqrt(cov[0][0])
-
-    values[1] = bf['delta_m^2']
-    rows[1] = 'delta_m^2'
-    #values[1]= bf['dmsL_over_E']
-    #rows[1] = 'dmsL_over_E'
-    uncertainty[1] = np.sqrt(cov[1][1])
-    #We don't report values for our nusiance parameters in this function
+    return True
     
-    vals = { 'value': values}
-    fit_values = pd.DataFrame(vals, index = rows)
-    fit_values['uncertainty'] = uncertainty
-    
-    return fit_values
-    
-def do_inference(data):
-    'an easy function to call that infers values of ss2t, dms, from a given data set'
-    
-    import matplotlib.pyplot as plt
-    best_fit, cov = fit_model(data)
-    data.plot(x='E', y='L', kind='scatter', yerr=0.5)
-    x = np.linspace(min(data['E']), max(data['E']), len(data['E']))
-
-    plt.plot(x, best_fit['prediction'], '-k', color='red')
-    print_fit_vals(best_fit, cov)
-    
-    return
--- bayboone/tests/test_module.py
+++ bayboone/tests/test_module.py
@@ -4,6 +4,8 @@ from unittest import TestCase
 import numpy as np
 import pandas as pd
 import pymc3 as pm
+import matplotlib.pyplot as plt
+import arviz as az
 
 from bayboone.inference import module
 
@@ -13,25 +15,51 @@ class TestOscillation_model(TestCase):
         test_model = module.oscillation_model(6000, 1)      
         self.assertTrue(isinstance(test_model, pm.model.Model))    
         
-    def test_accepts_resonable_inputs(self):        
-        num_numu, num_nue = (10, 22)        
-        #test_model = module.oscillation_model(num_numu, num_nue)       
+    def test_rejects_unresonable_inputs(self):        
+        num_numu, num_nue = (10, 22)          
         self.assertRaises(ValueError, module.oscillation_model, num_numu, num_nue)    
         
-class TestInference(TestCase):
+class TestInference(TestCase):    
     
-    def test_fit_model(self):
+    def test_fit_model_returns_trace(self):
         ss2t = 0.1 #unit-less
         dms = 1 #eV^2
         E = 1 #GeV
         L = 0.5 #km
         num_numu = 600000
         num_nue = num_numu*ss2t*np.sin(dms*L/E)**2
-        
-        best_fit, cov = module.fit_model([num_numu, num_nue])
-        
-        self.assertAlmostEqual(ss2t, best_fit['sin^2_2theta'])
-        self.assertAlmostEqual(dms, best_fit['delta_m^2'])
-        
     
+        trace = module.fit_model(num_numu, num_nue, 10)
+        self.assertTrue(isinstance(trace, az.InferenceData))    
         
+    def test_inference_returns_physical_values(self):
+        # We're still working on improving the statistical model to get one that's both correct and does the inference well
+        # (See ongoing discussion in group 6 slack channel)
+        # In the meantime, we'll just make sure it returns physical values
+        
+        ss2t = 0.1 #unit-less
+        dms = 1 #eV^2
+        E = 1 #GeV
+        L = 0.5 #km
+        num_numu = 600000
+        num_nue = num_numu*ss2t*np.sin(dms*L/E)**2
+    
+        trace = module.fit_model(num_numu, num_nue, 1000)
+        df_trace = pm.trace_to_dataframe(trace)
+        q = df_trace.quantile([0.16,0.50,0.84], axis=0)
+        df_trace['delta_m^2'] = np.exp(df_trace['log_delta_m^2'])
+        self.assertTrue(q['delta_m^2'][0.84]<1 and q['delta_m^2'][0.16]>0.01)
+        self.assertTrue(q['sin^2_2theta'][0.84]<1 and q['sin^2_2theta'][0.16]<0.0001)
+        
+    def test_prints_fitvals(self):
+        ss2t = 0.1 #unit-less
+        dms = 1 #eV^2
+        E = 1 #GeV
+        L = 0.5 #km
+        num_numu = 600000
+        num_nue = num_numu*ss2t*np.sin(dms*L/E)**2
+    
+        trace = module.fit_model(num_numu, num_nue, 10)
+        self.assertTrue(module.print_fit_vals(trace))
+    
+   
