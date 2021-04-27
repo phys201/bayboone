4e1d55644f47466cec20f8cfc65ee3c390ac93fc
jybook <julia.y.book@gmail.com>
2021-04-26 22:57:09 -0400

    Added Gaussian priors on ss2t, dms
    Gets the inference working, at least

--- bayboone/inference/module.py
+++ bayboone/inference/module.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import arviz as az
 # Adapted from week 07 notebook
 
-def oscillation_model(num_neutrinos, num_nue):
+def oscillation_model(num_neutrinos, num_nue, est_ss2t = 0.5, est_dms = 0.8):
     '''
     Creates a statistical model for predicting the oscillation parameters from microboone-like values
     Inputs:
@@ -13,6 +13,10 @@ def oscillation_model(num_neutrinos, num_nue):
         The number of muon neutrinos shot at the detector
     num_nue:
         The number of electron neutrinos detected
+    est_ss2t: float between 0 and 1
+        estimated ss2t from previous experiments, for use in the prior
+    est_dms: float above 0
+        estimated dms from previous experiments, also for use in prior
     Returns:
     osc_model: pymc3 model
         the statistical model for our neutrino oscillations in the 3+1 model
@@ -23,27 +27,31 @@ def oscillation_model(num_neutrinos, num_nue):
 
     osc_model = pm.Model()
     with osc_model:
-    
-        # Priors for unknown model parameters, covering the sensitivity region of modern experiments
-        ss2t = pm.Uniform('sin^2_2theta', 0.0001, 1)
-        #dms = pm.Uniform('delta_m^2', 0.01, 11.0) #units of ev^2
-        log_dms = pm.Uniform('log_delta_m^2', -4.605, 2.3)
-        # We don't know the exact energy or production point of each neutrino, so we draw from a truncated gaussian 
-        # (enforcing positive distance travelled and energy)
         
-        L = pm.TruncatedNormal('L', mu = 0.500, sigma = 0.05, lower = 0) #units of km
-        E = pm.TruncatedNormal('E', mu = 1.0, sigma = 0.05, lower = 0) #units of GeV
+        # We don't know the exact energy or production point of each neutrino, so we draw from a truncated gaussian 
+        # (enforcing positive distance travelled and energy)   
+        L = pm.TruncatedNormal('L', mu = 0.500, sigma = 0.15, lower = 0, upper = 0.6) #units of km
+        E = pm.TruncatedNormal('E', mu = 1.0, sigma = 0.15, lower = 0) #units of GeV
+        
+        # Priors for unknown model parameters, centered on a prior estimate of ss2t, dms
+        ss2t = pm.TruncatedNormal('sin^2_2theta', mu = est_ss2t, sigma = 0.1, lower = 0, upper = 1 ) #pm.Uniform('sin^2_2theta', 0.0001, 1)
+        dms = pm.TruncatedNormal('delta_m^2', mu = est_dms, sigma = 0.1, lower = 0, upper = E*np.pi/(1.27*L))
+        #This limits the inferred point to only the first of the delta m^2 values that would fit the function, eliminating periodicity.
+        
+        #pm.Uniform('delta_m^2', 0.01, 11.0) #units of ev^2
+        #log_dms = pm.Uniform('log_delta_m^2', -4.605, 2.3)
+        
 
         # In the large n limit, because the number of oscillations is low, we use a Poisson approximation
-        #rate = pm.Deterministic('rate', num_neutrinos*ss2t*(np.sin(dms*(1.27*L)/E))**2)
-        rate = pm.Deterministic('rate', num_neutrinos*ss2t*(np.sin(np.exp(log_dms)*(1.27*L)/E))**2)
+        rate = pm.Deterministic('rate', num_neutrinos*ss2t*(np.sin(dms*(1.27*L)/E))**2)
+        #rate = pm.Deterministic('rate', num_neutrinos*ss2t*(np.sin(np.exp(log_dms)*(1.27*L)/E))**2)
         
         #Likelihood of observations
         measurements = pm.Poisson('nue_flux', mu = rate, observed = num_nue)
         
     return osc_model
 
-def fit_model(num_neutrinos, num_nue, num_draws = 1000):
+def fit_model(num_neutrinos, num_nue, num_draws = 10000):
     '''
     Fits a given model to data provided using MCMC sampling
     
@@ -80,7 +88,7 @@ def print_fit_vals(trace):
     
     '''
     df_trace = pm.trace_to_dataframe(trace)
-    df_trace['delta_m^2'] = np.exp(df_trace['log_delta_m^2'])
+    #df_trace['delta_m^2'] = np.exp(df_trace['log_delta_m^2'])
     q = df_trace.quantile([0.16,0.50,0.84], axis=0)
     print("delta_m^2 = {:.2f} + {:.2f} - {:.2f}".format(q['delta_m^2'][0.50], 
                                             q['delta_m^2'][0.84]-q['delta_m^2'][0.50],
