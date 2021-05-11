9c3a3f77269662294fec178c3f518ddb64456035
tcontrer <taylorcontreras@g.harvard.edu>
2021-05-11 19:43:19 -0400

    Fixed variable mix up in data_io

--- bayboone/tests/test_io.py
+++ bayboone/tests/test_io.py
@@ -40,7 +40,7 @@ class TestSimulateData(TestCase):
         the seed to actually be consistent, so for now just testing they are
         integers.
         """
-        N_nue = Data.simulate_data(Data, N_numu=10, ss2t=1.0, dms=1.0, mu_L=0.5, mu_E=1.0, sigma_L=.025, sigma_E=0.25)
+        N_nue = Data.simulate_data(Data, ss2t=1.0, dms=1.0,  N_numu=10, mu_L=0.5, mu_E=1.0, sigma_L=.025, sigma_E=0.25)
         assert isinstance(N_nue, int)
         
     def test_bad_ss2t(self):
@@ -52,7 +52,7 @@ class TestSimulateData(TestCase):
         sigma_L = .1
         sigma_E = .1
         for bad_ss2t in bad_ss2t_list:
-            self.assertRaises(ValueError, Data.simulate_data, Data, N_numu, bad_ss2t, dms, L, E, sigma_L, sigma_E)
+            self.assertRaises(ValueError, Data.simulate_data, Data, bad_ss2t, dms, N_numu, L, E, sigma_L, sigma_E)
     
     def test_bad_dms(self):
         ss2t = 0.5
@@ -63,7 +63,7 @@ class TestSimulateData(TestCase):
         sigma_L = .1
         sigma_E = .1
         for bad_dms in bad_dms_list:
-            self.assertRaises(ValueError, Data.simulate_data, Data, N_numu, ss2t, bad_dms, L, E, sigma_L, sigma_E)
+            self.assertRaises(ValueError, Data.simulate_data, Data, ss2t, bad_dms, N_numu, L, E, sigma_L, sigma_E)
     
     def test_bad_N_numu(self):
         ss2t = 0.5
@@ -74,7 +74,7 @@ class TestSimulateData(TestCase):
         sigma_L = .1
         sigma_E = .1
         for bad_N_numu in bad_N_numu_list:
-            self.assertRaises(ValueError, Data.simulate_data, Data, bad_N_numu, ss2t, dms, L, E, sigma_L, sigma_E)
+            self.assertRaises(ValueError, Data.simulate_data, Data, ss2t, dms, bad_N_numu, L, E, sigma_L, sigma_E)
             
     def test_bad_L(self):
         ss2t = 0.5
@@ -85,7 +85,7 @@ class TestSimulateData(TestCase):
         sigma_L = .1
         sigma_E = .1
         for bad_L in bad_L_list:
-            self.assertRaises(ValueError, Data.simulate_data, Data, N_numu, ss2t, dms, bad_L, E, sigma_L, sigma_E)
+            self.assertRaises(ValueError, Data.simulate_data, Data, ss2t, dms, N_numu, bad_L, E, sigma_L, sigma_E)
     
     def test_bad_E(self):
         ss2t = 0.5
@@ -96,7 +96,7 @@ class TestSimulateData(TestCase):
         sigma_L = .1
         sigma_E = .1
         for bad_E in bad_E_list:
-            self.assertRaises(ValueError, Data.simulate_data, Data, N_numu, ss2t, dms, L, bad_E, sigma_L, sigma_E)
+            self.assertRaises(ValueError, Data.simulate_data, Data, ss2t, dms, N_numu, L, bad_E, sigma_L, sigma_E)
             
     def test_bad_sigma_L(self):
         ss2t = 0.5
@@ -107,7 +107,7 @@ class TestSimulateData(TestCase):
         bad_sigma_L_list = [-1., 0.]
         sigma_E = .1
         for bad_sigma_L in bad_sigma_L_list:
-            self.assertRaises(ValueError, Data.simulate_data, Data, N_numu, ss2t, dms, L, E, bad_sigma_L, sigma_E)
+            self.assertRaises(ValueError, Data.simulate_data, Data, ss2t, dms, N_numu, L, E, bad_sigma_L, sigma_E)
             
     def test_bad_sigma_E(self):
         ss2t = 0.5
@@ -118,7 +118,7 @@ class TestSimulateData(TestCase):
         sigma_L = .1
         bad_sigma_E_list = [-1., 0.]
         for bad_sigma_E in bad_sigma_E_list:
-            self.assertRaises(ValueError, Data.simulate_data, Data, N_numu, ss2t, dms, L, E, sigma_L, bad_sigma_E)
+            self.assertRaises(ValueError, Data.simulate_data, Data, ss2t, dms, N_numu, L, E, sigma_L, bad_sigma_E)
 
         
 class TestSimulateDetector(TestCase):
